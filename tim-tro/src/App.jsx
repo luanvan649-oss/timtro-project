80807a5f368e92b41efa66866871ed71ddd032cf
huyn2603 <ngochuyn029@gmail.com>
2025-11-22 20:27:08 +0700

    update code

--- tim-tro/src/App.jsx
+++ tim-tro/src/App.jsx
@@ -3,6 +3,7 @@ import React from 'react';
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import Login from './pages/Login';
 import Register from './pages/Register';
+import Profile from './pages/Profile';
 
 function App() {
   return (
@@ -10,7 +11,8 @@ function App() {
       <Routes>
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
-        <Route path="/" element={<Navigate to="/login" replace />} />
+        
+        <Route path="/profile" element={<Profile />} />
       </Routes>
     </BrowserRouter>
   );
